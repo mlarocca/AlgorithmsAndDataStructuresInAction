523c363a73954579492d004e71e5bbaeb4c55837
mlarocca <marcellolarocca@gmail.com>
2019-08-30 20:10:56 +0200

    Java: Huffman tests

--- Java/src/org/mlarocca/huffman/Huffman.java
+++ Java/src/org/mlarocca/huffman/Huffman.java
@@ -9,15 +9,14 @@ import java.util.*;
 public class Huffman {
 
     private Huffman() {
-
     }
 
-    public static Map<String, String> encode(String text) {
+    public static Map<Character, String> encode(String text) {
         return createEncoding(computeFrequenciesTree(createFrequenciesQueue(computeFrequencies(text))));
     }
 
     @VisibleForTesting
-    protected static Map<String, String> createEncoding(Node frequenciesTree) {
+    protected static Map<Character, String> createEncoding(Node frequenciesTree) {
         return frequenciesTree.createEncoding();
     }
 
@@ -26,7 +25,7 @@ public class Huffman {
         while (frequenciesQueue.size() > 1) {
             Optional<Node> right = frequenciesQueue.top();
             Optional<Node> left = frequenciesQueue.top();
-            List<String> symbols = new ArrayList<>(left.map(n -> n.symbols).orElse(Collections.EMPTY_LIST));
+            List<Character> symbols = new ArrayList<>(left.map(n -> n.symbols).orElse(Collections.EMPTY_LIST));
             symbols.addAll(right.map(n -> n.symbols).orElse(Collections.EMPTY_LIST));
             Double priority = left.map(n -> n.priority).orElse(0.) + right.map(n -> n.priority).orElse(0.);
             frequenciesQueue.add(new Node(symbols, priority, left, right), priority);
@@ -36,11 +35,11 @@ public class Huffman {
     }
 
     @VisibleForTesting
-    protected static PriorityQueue<Node> createFrequenciesQueue(Map<String, Long> frequencies) {
+    protected static PriorityQueue<Node> createFrequenciesQueue(Map<Character, Long> frequencies) {
         List<Node> elements = new ArrayList<>();
         List<Double> priorities = new ArrayList<>();
 
-        for (Map.Entry<String, Long> entry : frequencies.entrySet()) {
+        for (Map.Entry<Character, Long> entry : frequencies.entrySet()) {
             double priority = entry.getValue().doubleValue();
             elements.add(new Node(Arrays.asList(entry.getKey()), priority, Optional.empty(), Optional.empty()));
             priorities.add(priority);
@@ -50,11 +49,11 @@ public class Huffman {
     }
 
     @VisibleForTesting
-    protected static Map<String, Long> computeFrequencies(String text) {
-        Map<String, Long> frequencyMap = new HashMap<>();
+    protected static Map<Character, Long> computeFrequencies(String text) {
+        Map<Character, Long> frequencyMap = new HashMap<>();
         for (Character c : text.toCharArray()) {
-            frequencyMap.putIfAbsent(c.toString(), 0L);
-            frequencyMap.compute(c.toString(), (key, freq) -> freq + 1);
+            frequencyMap.putIfAbsent(c, 0L);
+            frequencyMap.compute(c, (key, freq) -> freq + 1L);
         }
 
         return frequencyMap;
@@ -62,29 +61,41 @@ public class Huffman {
 
     @VisibleForTesting
     protected static class Node {
-        private final List<String> symbols;
+        private final List<Character> symbols;
         private final Double priority;
         private final Optional<Node> left;
         private final Optional<Node> right;
 
-        public Node(List<String> symbols, Double priority, Optional<Node> left, Optional<Node> right) {
+        public Node(List<Character> symbols, Double priority, Optional<Node> left, Optional<Node> right) {
             this.symbols = symbols;
             this.priority = priority;
             this.left = left;
             this.right = right;
         }
 
-        public Map<String, String> createEncoding() {
+        @VisibleForTesting
+        protected List<Character> getSymbols() {
+            // Make a copy to avoid leaking access to internal fields;
+            return new ArrayList<>(this.symbols);
+        }
 
-            Map<String, String> leftEncodingTable = left.map(Node::createEncoding).orElse(Collections.EMPTY_MAP);
-            Map<String, String> rightEncodingTable = right.map(Node::createEncoding).orElse(Collections.EMPTY_MAP);
+        @VisibleForTesting
+        protected double getPriority() {
+            // Auto unboxing
+            return this.priority;
+        }
 
-            Map<String, String> encodingTable = new HashMap<>();
+        public Map<Character, String> createEncoding() {
 
-            for (Map.Entry<String, String> entry : leftEncodingTable.entrySet()) {
+            Map<Character, String> leftEncodingTable = left.map(Node::createEncoding).orElse(Collections.EMPTY_MAP);
+            Map<Character, String> rightEncodingTable = right.map(Node::createEncoding).orElse(Collections.EMPTY_MAP);
+
+            Map<Character, String> encodingTable = new HashMap<>();
+
+            for (Map.Entry<Character, String> entry : leftEncodingTable.entrySet()) {
                 encodingTable.put(entry.getKey(), encodeLeftPath(entry.getValue()));
             }
-            for (Map.Entry<String, String> entry : rightEncodingTable.entrySet()) {
+            for (Map.Entry<Character, String> entry : rightEncodingTable.entrySet()) {
                 encodingTable.put(entry.getKey(), encodeRightPath(entry.getValue()));
             }
 
--- Java/tests/org/mlarocca/huffman/HuffmanTest.java
+++ Java/tests/org/mlarocca/huffman/HuffmanTest.java
@@ -1,13 +1,38 @@
 package org.mlarocca.huffman;
 
 import org.junit.Test;
+import org.mlarocca.containers.priorityqueue.PriorityQueue;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by marce on 14/11/2017.
- */
 public class HuffmanTest {
+    private static final String Text =
+            "fffeeeeeddddddcccccccbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+    private static final Map<Character, Long> ExpectedFrequenciesMap = new HashMap<>();
+    private static final Map<Character, String> ExpectedEncoding = new HashMap<>();
+
+    private static final List<Character> ExpectedSymbols = Arrays.asList('f', 'e', 'd', 'c', 'b', 'a');
+
+    static {
+        ExpectedFrequenciesMap.put('a', 57L);
+        ExpectedFrequenciesMap.put('b', 22L);
+        ExpectedFrequenciesMap.put('c', 7L);
+        ExpectedFrequenciesMap.put('d', 6L);
+        ExpectedFrequenciesMap.put('e', 5L);
+        ExpectedFrequenciesMap.put('f', 3L);
+
+        ExpectedEncoding.put('a', "0");
+        ExpectedEncoding.put('b', "10");
+        ExpectedEncoding.put('c', "1100");
+        ExpectedEncoding.put('d', "1101");
+        ExpectedEncoding.put('e', "1110");
+        ExpectedEncoding.put('f', "1111");
+    };
+
     @Test
     public void encode() throws Exception {
 
@@ -15,22 +40,55 @@ public class HuffmanTest {
 
     @Test
     public void createEncoding() throws Exception {
-
+        Huffman.Node root = Huffman.computeFrequenciesTree(
+                Huffman.createFrequenciesQueue(
+                        Huffman.computeFrequencies(Text)));
+        assertEquals(ExpectedEncoding, Huffman.createEncoding(root));
     }
 
     @Test
     public void computeFrequenciesTree() throws Exception {
+        Huffman.Node root = Huffman.computeFrequenciesTree(
+                Huffman.createFrequenciesQueue(
+                        Huffman.computeFrequencies(Text)));
 
+        assertEquals(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), root.getSymbols());
+        assertEquals(100.0, root.getPriority(), 0.001);
     }
 
     @Test
     public void createFrequenciesQueue() throws Exception {
+        PriorityQueue<Huffman.Node> queue = Huffman.createFrequenciesQueue(
+                Huffman.computeFrequencies(Text));
+        List<Double> charFrequencies = new ArrayList<>();
+        List<List<Character>> charSymbols = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Optional<Huffman.Node> node = queue.top();
+            assertTrue(node.isPresent());
+            charFrequencies.add(node.get().getPriority());
+            charSymbols.add(node.get().getSymbols());
+        }
 
+        // Frequencies should be sorted from smaller to larger
+        List<Double> expectedFrequencies = ExpectedFrequenciesMap.values().stream()
+                .map(i -> i.doubleValue())
+                .sorted()
+                .collect(Collectors.toList());
+
+        assertEquals(expectedFrequencies, charFrequencies);
+
+        // Likewise, symbols should be sorted by their document frequency
+        List<List<Character>> expectedSymbols = ExpectedFrequenciesMap.entrySet().stream()
+                .sorted((o1, o2) -> o1.getValue() <= o2.getValue() ? -1 : 1)
+                .map(entry -> Arrays.asList(entry.getKey()))
+                .collect(Collectors.toList());
+        assertEquals(expectedSymbols, charSymbols);
     }
 
     @Test
     public void computeFrequencies() throws Exception {
-
+        Map<Character, Long> frequencies = Huffman.computeFrequencies(Text);
+        assertEquals(ExpectedFrequenciesMap, frequencies);
     }
 
 }
