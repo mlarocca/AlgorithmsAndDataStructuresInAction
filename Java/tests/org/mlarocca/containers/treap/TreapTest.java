a9426b68a0b3824dfc7281e54f6654eee0ac467f
mlarocca <marcellolarocca@gmail.com>
2019-11-28 19:44:32 +0100

    Init - Temp

---
+++ Java/tests/org/mlarocca/containers/treap/TreapTest.java
@@ -0,0 +1,72 @@
+package org.mlarocca.containers.treap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TreapTest {
+
+    @Test
+    public void top() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void contains() {
+    }
+
+    @Test
+    public void priority() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void updatePriority() {
+    }
+
+    @Test
+    public void addElementOrUpdatePriority() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void min() {
+    }
+
+    @Test
+    public void max() {
+    }
+
+    @Test
+    public void search() {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
